01977b71f33ee6155280c42cd979acdaf9e216fc
morteza shojaei <mshojaei179@gmail.com>
2021-12-12 22:16:47 +0330

    complete persian translation

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,32 +1,46 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { GameProvider, useGame, usePlayer } from "../GameContext";
-import { PlayerIndex, PlayingCard, PlayingCardSuite, TGameData, TGameID } from "../../common/types";
+import {
+  PlayerIndex,
+  PlayingCard,
+  PlayingCardSuite,
+  TGameData,
+} from "../../common/types";
 import CardSet from "../components/CardSet";
 import CardSuite from "../components/CardSuite";
 import { canCardBePlayed, isNameValid } from "../../common/utils";
 import useGameUpdate from "../SocketContext";
 import Player from "../components/Player";
-import { Alert, Box, Button, Container, Divider, Grid, LinearProgress, TextField } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  LinearProgress,
+  TextField,
+} from "@mui/material";
 import Card from "../components/Card";
 import CenterContainer from "../components/CenterContainer";
-import BoltIcon from '@mui/icons-material/Bolt';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import BoltIcon from "@mui/icons-material/Bolt";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { useCookies } from "react-cookie";
 import SelectableItem from "../components/SelectableItem";
 import useApi from "../useApi";
+import { useTranslation } from "react-i18next";
 
 interface RouteParams {
   gameId: string;
 }
 
-
 const InPlayView: React.FC = () => {
   const game = useGame();
-  const api = useApi(game.game_id, 'play');
+  const api = useApi(game.game_id, "play");
 
   const canPlayCard = function (card: PlayingCard): boolean {
-    if (game.game_status !== 'in_play') {
+    if (game.game_status !== "in_play") {
       return false;
     }
 
@@ -38,42 +52,76 @@ const InPlayView: React.FC = () => {
   };
 
   const playerProps = { lead: game.whos_lead, highlighted: game.whos_turn };
-  const who_started_this_round = ((game.whos_turn ?? 0) - game.floor_cards.length + 4) % 4;
+  const who_started_this_round =
+    ((game.whos_turn ?? 0) - game.floor_cards.length + 4) % 4;
 
   return (
     <>
-      {game.your_index >= 0 ? <Box>
-        <Alert severity="info">
-          Welcome
-          <Player name={game.players[game.your_index]} index={game.your_index} {...playerProps} />
-        </Alert>
-      </Box> : <Box>
-        <Alert severity="info">This game already has all it's players — you can still watch it</Alert>
-      </Box>}
+      {game.your_index >= 0 ? (
+        <Box>
+          <Alert severity="info">
+            Welcome
+            <Player
+              name={game.players[game.your_index]}
+              index={game.your_index}
+              {...playerProps}
+            />
+          </Alert>
+        </Box>
+      ) : (
+        <Box>
+          <Alert severity="info">
+            This game already has all it's players — you can still watch it
+          </Alert>
+        </Box>
+      )}
       <Box sx={{ py: 2 }}>
         {/* PLAYERS AND TEAMS */}
-        <Grid container spacing={2} textAlign='center' sx={{ pb: 8 }}>
+        <Grid container spacing={2} textAlign="center" sx={{ pb: 8 }}>
           <Grid item xs={12} md={8}>
-            {[0, 1, 2, 3].map(index => <Player name={game.players[index]} index={index as PlayerIndex} {...playerProps} />)}
+            {[0, 1, 2, 3].map((index) => (
+              <Player
+                name={game.players[index]}
+                index={index as PlayerIndex}
+                {...playerProps}
+              />
+            ))}
           </Grid>
           <Grid item xs={12} md={4}>
-            <Player name="Team A" index={0 as PlayerIndex} score={game.wins[0]} />
-            {game.scoreline.join(' — ')}
-            <Player name="Team B" index={1 as PlayerIndex} score={game.wins[1]} />
+            <Player
+              name="Team A"
+              index={0 as PlayerIndex}
+              score={game.wins[0]}
+            />
+            {game.scoreline.join(" — ")}
+            <Player
+              name="Team B"
+              index={1 as PlayerIndex}
+              score={game.wins[1]}
+            />
           </Grid>
         </Grid>
 
         <Divider />
 
         {/* TRUMP SUITE */}
-        <Box sx={{ textAlign: 'center', mt: -1.7 }}>
-          {[0, 1, 2, 3].map(suite => <CardSuite suite={suite as PlayingCardSuite} highlighted={game.trump_suite} />)}
+        <Box sx={{ textAlign: "center", mt: -1.7 }}>
+          {[0, 1, 2, 3].map((suite) => (
+            <CardSuite
+              suite={suite as PlayingCardSuite}
+              highlighted={game.trump_suite}
+            />
+          ))}
         </Box>
 
         {/* PLAYER CARDS */}
-        <Box sx={{ textAlign: 'center', py: 2 }}>
-          <CardSet cards={game.your_cards} sort={true}
-            onSelectFactory={card => canPlayCard(card) ? () => api.send({ card }) : null}
+        <Box sx={{ textAlign: "center", py: 2 }}>
+          <CardSet
+            cards={game.your_cards}
+            sort={true}
+            onSelectFactory={(card) =>
+              canPlayCard(card) ? () => api.send({ card }) : null
+            }
             disabled={api.pending}
           />
         </Box>
@@ -81,75 +129,120 @@ const InPlayView: React.FC = () => {
         <Divider />
 
         {/* FLOOR CARDS */}
-        {game.game_status !== 'in_play' ? null : <Box sx={{ textAlign: 'center', py: 2 }}>
-          {[0, 1, 2, 3].map(ix => <Box display="inline-block">
-            <Box flexDirection="row">
-              <Box>
-                <Player name={game.players[(who_started_this_round + ix) % 4]} index={(who_started_this_round + ix) % 4 as PlayerIndex} {...playerProps} />
-              </Box>
-              <Box>
-                <Card card={game.floor_cards[ix]} />
-              </Box>
-            </Box>
-          </Box>)}
-        </Box>}
-
-        {/* GAME ACTIONS */}
-        {game.game_status !== 'finished' ? null : <>
-          <Divider />
-          <Container maxWidth="sm" sx={{ py: 4 }}>
-            <ResetGamePane />
-          </Container>
-        </>}
-        {game.game_status !== 'awaiting_trump_suite' ? null : (game.your_index === game.whos_turn ? <ChooseTrumpSuiteView /> : <AwaitingTrumpSuiteView />)}
-
-        {/* TRICKS */}
-        {game.tricks.length < 1 ? null : <>
-          <Divider />
-          <Box sx={{ textAlign: 'center', py: 2 }}>
-            {game.tricks.map(({ starting_player, winner, cards }) => <Box sx={{ mb: 2 }}>
-              {cards.map((card, ix) => <Box display="inline-block">
+        {game.game_status !== "in_play" ? null : (
+          <Box sx={{ textAlign: "center", py: 2 }}>
+            {[0, 1, 2, 3].map((ix) => (
+              <Box display="inline-block">
                 <Box flexDirection="row">
                   <Box>
-                    <Player name={game.players[(starting_player + ix) % 4]} index={(starting_player + ix) % 4 as PlayerIndex} highlighted={winner} lead={game.whos_lead} />
+                    <Player
+                      name={game.players[(who_started_this_round + ix) % 4]}
+                      index={((who_started_this_round + ix) % 4) as PlayerIndex}
+                      {...playerProps}
+                    />
                   </Box>
                   <Box>
-                    <Card card={card} highlighted={winner === (starting_player + ix) % 4} />
+                    <Card card={game.floor_cards[ix]} />
                   </Box>
                 </Box>
-              </Box>)}
-            </Box>)}
+              </Box>
+            ))}
           </Box>
-        </>}
+        )}
+
+        {/* GAME ACTIONS */}
+        {game.game_status !== "finished" ? null : (
+          <>
+            <Divider />
+            <Container maxWidth="sm" sx={{ py: 4 }}>
+              <ResetGamePane />
+            </Container>
+          </>
+        )}
+        {game.game_status !==
+        "awaiting_trump_suite" ? null : game.your_index === game.whos_turn ? (
+          <ChooseTrumpSuiteView />
+        ) : (
+          <AwaitingTrumpSuiteView />
+        )}
+
+        {/* TRICKS */}
+        {game.tricks.length < 1 ? null : (
+          <>
+            <Divider />
+            <Box sx={{ textAlign: "center", py: 2 }}>
+              {game.tricks.map(({ starting_player, winner, cards }) => (
+                <Box sx={{ mb: 2 }}>
+                  {cards.map((card, ix) => (
+                    <Box display="inline-block">
+                      <Box flexDirection="row">
+                        <Box>
+                          <Player
+                            name={game.players[(starting_player + ix) % 4]}
+                            index={((starting_player + ix) % 4) as PlayerIndex}
+                            highlighted={winner}
+                            lead={game.whos_lead}
+                          />
+                        </Box>
+                        <Box>
+                          <Card
+                            card={card}
+                            highlighted={winner === (starting_player + ix) % 4}
+                          />
+                        </Box>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+              ))}
+            </Box>
+          </>
+        )}
       </Box>
     </>
   );
 };
 
-
-
 const ResetGamePane: React.FC = () => {
+  const { t } = useTranslation();
   const game = useGame();
-  const api = useApi(game.game_id, 'reset');
+  const api = useApi(game.game_id, "reset");
 
   if (game.next_game_id != null) {
-    return <Button
-      endIcon={<ChevronRightIcon />}
-      href={`/game/${game.next_game_id}`}
-      variant="contained">Continue to Next Game!</Button>;
+    return (
+      <Button
+        endIcon={<ChevronRightIcon />}
+        href={`/game/${game.next_game_id}`}
+        variant="contained"
+      >
+        {t("continue-to-next-game")}
+      </Button>
+    );
   }
 
   if (game.whos_admin !== game.your_index) {
-    return <Alert severity="info">Admin of this game can create the next game.</Alert>;
+    return (
+      <Alert severity="info">
+        {t("admin-of-this-game-can-create-the-next-game")}
+      </Alert>
+    );
   }
 
   return (
     <Box>
       <Box sx={{ py: 2 }}>
-        <Alert severity="info">You're the admin and you can create the next game!</Alert>
+        <Alert severity="info">
+          {t("you-are-the-admin-and-you-can-create-the-next-game")}
+        </Alert>
       </Box>
-      <Button variant="contained" startIcon={<BoltIcon />} onClick={api.send}
-        disabled={api.pending}>Create the Next Game!</Button>
+      <Button
+        variant="contained"
+        startIcon={<BoltIcon />}
+        onClick={api.send}
+        disabled={api.pending}
+      >
+        {t("create-the-next-game")}
+      </Button>
     </Box>
   );
 };
@@ -158,20 +251,23 @@ const AwaitingPlayersView: React.FC = () => {
   const { your_index, players } = useGame();
   return (
     <CenterContainer>
-      <Box textAlign='center' sx={{ mb: 4 }}>
-        {players.map((name, ix) => <Player name={name} index={ix as PlayerIndex} />)}
+      <Box textAlign="center" sx={{ mb: 4 }}>
+        {players.map((name, ix) => (
+          <Player name={name} index={ix as PlayerIndex} />
+        ))}
       </Box>
       {your_index < 0 ? <JoinGameView /> : <AwaitingMorePlayersView />}
     </CenterContainer>
   );
-}
+};
 
 const JoinGameView: React.FC = () => {
-  const [cookies, setCookie] = useCookies(['player_name']);
-  const defaultName = cookies.player_name ?? '';
+  const { t } = useTranslation();
+  const [cookies, setCookie] = useCookies(["player_name"]);
+  const defaultName = cookies.player_name ?? "";
   const [selectedName, setSelectedName] = useState(defaultName);
   const game = useGame();
-  const api = useApi(game.game_id, 'join');
+  const api = useApi(game.game_id, "join");
 
   const invalid_form = !isNameValid(selectedName);
 
@@ -180,36 +276,51 @@ const JoinGameView: React.FC = () => {
       <Box>
         <TextField
           error={selectedName.length > 0 && invalid_form}
-          helperText="Only characters and numbers, 3 to 8 characters long"
-          label="Pick a name..."
+          helperText={t("name-field-helper")}
+          label={t("pick-a-name")}
           defaultValue={defaultName}
-          sx={{ width: '100%' }}
+          sx={{ width: "100%" }}
           onChange={({ target }) => setSelectedName(target.value)}
         />
       </Box>
       <Box sx={{ mt: 4 }}>
-        <Button variant="contained" onClick={() => {
-          api.send({ name: selectedName });
-          setCookie('player_name', selectedName, { path: '/', maxAge: 31622400 });
-        }}
-          disabled={api.pending || invalid_form}>Join This Game!</Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            api.send({ name: selectedName });
+            setCookie("player_name", selectedName, {
+              path: "/",
+              maxAge: 31622400,
+            });
+          }}
+          disabled={api.pending || invalid_form}
+        >
+          {t("join-this-game")}
+        </Button>
       </Box>
     </>
   );
 };
 
 const AwaitingMorePlayersView: React.FC = () => {
+  const { t } = useTranslation();
   const game = useGame();
   return (
     <>
       <Box sx={{ my: 4 }}>
-        <Alert severity="info">Waiting for <b>{4 - game.players.length} more player</b> to join...</Alert>
+        <Alert severity="info">
+          {t("waiting-for")}
+          <b>
+            {4 - game.players.length} {t("more-player")}
+          </b>{" "}
+          {t("to-join")}
+        </Alert>
       </Box>
       <TextField
-        label="Share this link with your friends..."
+        label={t("share-this-link-with-your-friends")}
         defaultValue={window.location.href}
-        sx={{ width: '100%' }}
-        onFocus={event => event.target.select()}
+        sx={{ width: "100%" }}
+        onFocus={(event) => event.target.select()}
         InputProps={{ readOnly: true }}
       />
     </>
@@ -217,33 +328,49 @@ const AwaitingMorePlayersView: React.FC = () => {
 };
 
 const ChooseTrumpSuiteView: React.FC = () => {
+  const { t } = useTranslation();
   const game = useGame();
-  const api = useApi(game.game_id, 'choose_trump_suite');
+  const api = useApi(game.game_id, "choose_trump_suite");
 
   return (
     <Box sx={{ m: 4 }}>
       <Box sx={{ my: 2 }}>
-        <Alert severity="info">Please choose a trump suite:</Alert>
+        <Alert severity="info">{t("please-choose-a-trump-suite")}</Alert>
       </Box>
-      <Box sx={{ textAlign: 'center' }}>
-        {[0, 1, 2, 3].map(suite => (<SelectableItem
-          onSelect={() => api.send({ trump_suite: suite })}
-          disabled={api.pending}
-        >
-          <Box sx={{ mx: 1, px: 1, py: 3, border: 'solid 1px #ccc', borderRadius: '1rem' }}>
-            <CardSuite suite={suite as PlayingCardSuite} />
-          </Box>
-        </SelectableItem>))}
+      <Box sx={{ textAlign: "center" }}>
+        {[0, 1, 2, 3].map((suite) => (
+          <SelectableItem
+            onSelect={() => api.send({ trump_suite: suite })}
+            disabled={api.pending}
+          >
+            <Box
+              sx={{
+                mx: 1,
+                px: 1,
+                py: 3,
+                border: "solid 1px #ccc",
+                borderRadius: "1rem",
+              }}
+            >
+              <CardSuite suite={suite as PlayingCardSuite} />
+            </Box>
+          </SelectableItem>
+        ))}
       </Box>
     </Box>
   );
 };
 
 const AwaitingTrumpSuiteView: React.FC = () => {
+  const { t } = useTranslation();
   const { whos_lead, players } = useGame();
   return (
     <Box sx={{ m: 4 }}>
-      <Alert severity="info">Waiting for <Player name={players[whos_lead]} index={whos_lead} lead={whos_lead} /> to choose trump suite...</Alert>
+      <Alert severity="info">
+        {t("waiting-for")}
+        <Player name={players[whos_lead]} index={whos_lead} lead={whos_lead} />
+        {t("to-choose-trump-suite")}
+      </Alert>
     </Box>
   );
 };
@@ -252,16 +379,17 @@ const GameView: React.FC = () => {
   const game = useGame();
   const { game_status } = game;
   switch (game_status) {
-    case 'awaiting_trump_suite':
-    case 'in_play':
-    case 'finished':
+    case "awaiting_trump_suite":
+    case "in_play":
+    case "finished":
       return <InPlayView />;
-    case 'awaiting_players':
+    case "awaiting_players":
       return <AwaitingPlayersView />;
   }
 };
 
 export const GameScreen: React.FC = () => {
+  const { t } = useTranslation();
   const { gameId } = useParams<RouteParams>();
   const { updateToken } = useGameUpdate(gameId);
   const api = useApi<TGameData>(gameId, null);
@@ -271,13 +399,23 @@ export const GameScreen: React.FC = () => {
   }, [updateToken]);
 
   if (!api.pending && api.data == null) {
-    return <CenterContainer><Alert severity='error'>Oops! Something went wrong! Error Code: {api.error_code}</Alert></CenterContainer>;
+    return (
+      <CenterContainer>
+        <Alert severity="error">
+          {t("error-in-api-response")} {api.error_code}
+        </Alert>
+      </CenterContainer>
+    );
   }
 
-  return (<>
-    <LinearProgress sx={{ opacity: api.pending ? 1 : 0 }} />
-    {api.data == null ? null : <GameProvider value={{ ...api.data, game_id: gameId }}>
-      <GameView />
-    </GameProvider>}
-  </>);
+  return (
+    <>
+      <LinearProgress sx={{ opacity: api.pending ? 1 : 0 }} />
+      {api.data == null ? null : (
+        <GameProvider value={{ ...api.data, game_id: gameId }}>
+          <GameView />
+        </GameProvider>
+      )}
+    </>
+  );
 };
