ddafb857488e24dd4c252235b5049e6d25a3f7a9
Mo Valipour <mov@fb.com>
2021-12-17 15:08:53 +0000

    add developer hub link

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,20 @@ import Player from "./components/Player";
 ReactDOM.render(
   <React.StrictMode>
     <Container fixed sx={{ px: 0 }}>
-      <Box sx={{ px: 1, py: 2 }}>
-        <a href='/'><img src="/logo.svg" alt="Hakem.club" style={{ height: '1.5rem' }} /></a>
-      </Box>
+      <Grid container spacing={2} sx={{ p: 1, color: '#aaa', alignItems: 'center' }} className="app-header">
+        <Grid item xs={12} md={6}>
+          <Box sx={{ px: 1, py: 2 }}>
+            <a href='/' className="logo-link"><img src="/logo.svg" alt="Hakem.club" style={{ height: '1.5rem' }} /></a>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} textAlign='right'>
+          <a target="_blank" href='https://hakem-club.github.io/'>Developer Hub</a>
+        </Grid>
+      </Grid>
       <Box sx={{ boxShadow: '0 3px 10px rgb(0 0 0 / 5%)', backgroundColor: 'white' }}>
         <App />
       </Box>
-      <Grid container spacing={2} sx={{ p: 1, color: '#aaa' }}>
+      <Grid container spacing={2} sx={{ p: 1, color: '#aaa' }} className="app-footer">
         <Grid item xs={12} md={6}>
           open source client: <a href='https://github.com/hakem-club/courtpiece-web'>@hakem-club/courtpiece-web</a>
         </Grid>
