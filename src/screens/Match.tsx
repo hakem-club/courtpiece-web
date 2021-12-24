fd8a58dd19d4e06b7d864988f8b0a598fa401efc
Mo Valipour <mov@fb.com>
2021-12-24 22:20:15 +0000

    match: reset

--- src/screens/Match.tsx
+++ src/screens/Match.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, IconButton, LinearProgress, Step, StepLabel, Stepper } from "@mui/material";
+import { Alert, Box, Button, Divider, IconButton, LinearProgress, Step, StepLabel, Stepper } from "@mui/material";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { PlayerIndex, TMatchData, TMatchID } from "../../common/types";
@@ -7,6 +7,7 @@ import Player from "../components/Player";
 import useGameUpdate from "../SocketContext";
 import { useMatchApi } from "../useApi";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 interface RouteParams {
     matchId: TMatchID;
@@ -16,6 +17,7 @@ export const MatchScreen: React.FC = () => {
     const { matchId } = useParams<RouteParams>();
     const { updateToken } = useGameUpdate(matchId);
     const api = useMatchApi<TMatchData>(matchId, null);
+    const resetApi = useMatchApi<{ match_id: TMatchID }>(matchId, 'reset');
 
     useEffect(() => {
         api.send();
@@ -26,34 +28,47 @@ export const MatchScreen: React.FC = () => {
     }
 
     return (<>
-        <LinearProgress sx={{ opacity: api.pending ? 1 : 0, mb: -0.5 }} />
-        {api.data == null ? null : <>
-            <Stepper activeStep={api.data.finished_games.length + 1} orientation="vertical" sx={{ p: 4 }}>
-                {api.data.finished_games.map((game, ix) => <Step key={ix}>
-                    <StepLabel>
-                        <Player name="Team A" index={0 as PlayerIndex} score={game.wins[0]} highlighted={game.winner_team} />
-                        {game.scoreline.join(' — ')}
-                        <Player name="Team B" index={1 as PlayerIndex} score={game.wins[1]} highlighted={game.winner_team} />
-                        <Box sx={{ mx: 2 }} display="inline-block">
-                            Game #{ix+1}
-                            <IconButton aria-label="delete" size="small" href={`/game/${game.id}`} target="_blank">
-                                <OpenInNewIcon fontSize="inherit" />
-                            </IconButton>
-                        </Box>
-                    </StepLabel>
-                </Step>)}
-                {api.data.current_game == null ? null : <Step>
-                    <Player name="Team A" index={0 as PlayerIndex} score={api.data.current_game.wins[0]} />
-                    {api.data.current_game.status}
-                    <Player name="Team B" index={1 as PlayerIndex} score={api.data.current_game.wins[1]} />
-                    <Box sx={{ mx: 2 }} display="inline-block">
-                        Game #{api.data.finished_games.length+1}
-                        <IconButton aria-label="delete" size="small" href={`/game/${api.data.current_game.id}`} target="_blank">
-                            <OpenInNewIcon fontSize="inherit" />
-                        </IconButton>
-                    </Box>
-                </Step>}
-            </Stepper>
-        </>}
+        <LinearProgress sx={{ opacity: api.pending || resetApi.pending ? 1 : 0, mb: -0.5 }} />
+        <Box sx={{ p: 4 }}>
+            {api.data == null ? null : <>
+                <Stepper activeStep={api.data.finished_games.length} orientation="vertical">
+                    {api.data.finished_games.map((game, ix) => <Step key={ix}>
+                        <StepLabel>
+                            <Player name="Team A" index={0 as PlayerIndex} score={game.wins[0]} highlighted={game.winner_team} />
+                            {game.scoreline.join(' — ')}
+                            <Player name="Team B" index={1 as PlayerIndex} score={game.wins[1]} highlighted={game.winner_team} />
+                            <Box sx={{ mx: 2 }} display="inline-block">
+                                Game #{ix + 1}
+                                <IconButton aria-label="delete" size="small" href={`/game/${game.id}`} target="_blank">
+                                    <OpenInNewIcon fontSize="inherit" />
+                                </IconButton>
+                            </Box>
+                        </StepLabel>
+                    </Step>)}
+                    {api.data.current_game == null ? null : <Step>
+                        <StepLabel>
+                            <Player name="Team A" index={0 as PlayerIndex} score={api.data.current_game.wins[0]} />
+                            {api.data.current_game.status}
+                            <Player name="Team B" index={1 as PlayerIndex} score={api.data.current_game.wins[1]} />
+                            <Box sx={{ mx: 2 }} display="inline-block">
+                                Game #{api.data.finished_games.length + 1}
+                                <IconButton aria-label="delete" size="small" href={`/game/${api.data.current_game.id}`} target="_blank">
+                                    <OpenInNewIcon fontSize="inherit" />
+                                </IconButton>
+                            </Box>
+                        </StepLabel>
+                    </Step>}
+                </Stepper>
+                <Divider sx={{ my: 2 }} />
+                {api.data.current_game != null ? null : <>
+                    {resetApi.data == null ? (<Button variant="contained" onClick={() => resetApi.send()} disabled={resetApi.pending}>
+                        Restart!
+                    </Button>) : (<Button
+                        endIcon={<ChevronRightIcon />}
+                        href={`/match/${resetApi.data.match_id}`}
+                        variant="contained">Continue to Next Match!</Button>)
+                    }</>}
+            </>}
+        </Box>
     </>);
 };
